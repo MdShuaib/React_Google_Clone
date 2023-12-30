f1cd381cca3445a08d81a184371b3f5bf1d3c140
md_shuaib <mshuaibin@gmail.com>
2023-12-30 21:23:43 +0530

    app header alignment with material ui.

---
+++ src/pages/Home.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Home.css';
+import { Link } from 'react-router-dom';
+import AppsIcon from '@material-ui/icons/Apps';
+import { Avatar } from '@material-ui/core';
+const Home = () => {
+    return (
+        <div className='home'>
+            <div className='home__header'>
+                <div className='home__headerLeft'>
+                    <Link to={'/about'}>About</Link>
+                    <Link to={'/store'}>Store</Link>
+                </div>
+
+                <div className='home__headerRight'>
+                    <Link to={'/gmail'}>Gmail</Link>
+                    <Link to={'/images'}>Images</Link>
+                    <AppsIcon />
+                    <Avatar />
+                </div>
+            </div>
+
+            <div className='home__body'>
+
+            </div>
+        </div>
+    );
+}
+
+export default Home;
