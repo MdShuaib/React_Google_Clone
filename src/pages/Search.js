456add4b1a8d534aa28ca38c731602bf38f07aa2
md_shuaib <mshuaibin@gmail.com>
2023-12-31 08:41:40 +0530

    Hide buttons on search component.

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -3,27 +3,41 @@ import './Search.css';
 import SearchIcon from '@material-ui/icons/Search';
 import MicIcon from '@material-ui/icons/Mic';
 import { Button } from '@material-ui/core';
+import { useNavigate } from 'react-router-dom';
 
 
 
 
-function Search() {
+function Search({ hideButtons = false }) {
     const [input, setInput] = useState('');
+    let navigate = useNavigate();
+
     const search = e => {
         e.preventDefault();
+        navigate("/search");
     }
     return (
-        <div className='search'>
+        <form className='search'>
             <div className='search__input'>
                 <SearchIcon className="search_inputIcon" />
                 <input value={input} onChange={e => setInput(e.target.value)} />
                 <MicIcon />
             </div>
+            {!hideButtons ? (
             <div className='search__buttons'>
-                <Button onClick={search} variant='outlined'>Google Search</Button>
+                <Button type='submit' onClick={search} variant='outlined'>Google Search</Button>
                 <Button variant='outlined'>I'm Feeling Lucky</Button>
             </div>
-        </div>
+            ) : (
+            <div className='search__buttons'>
+                <Button className='search__buttonHidden'
+                    type='submit' onClick={search} variant='outlined'>Google Search</Button>
+                <Button className='search__buttonHidden'
+                    variant='outlined'>I'm Feeling Lucky</Button>
+            </div>
+            )}
+
+        </form>
     );
 }
 
