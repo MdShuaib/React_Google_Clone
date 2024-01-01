edc6c0da68224d68030d521e7e132415060a355c
md_shuaib <mshuaibin@gmail.com>
2024-01-01 19:00:35 +0530

    Fixed searchpage icons alignment

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -25,7 +25,7 @@ const Home = () => {
             <div className='home__body'>
                 <img src='https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png' alt='' />
                 <div className='home_inputContainer'>
-                    <Search hideButtons />
+                    <Search />
                 </div>
             </div>
         </div>
--- src/pages/Searchpage.js
+++ src/pages/Searchpage.js
@@ -3,23 +3,72 @@ import './Searchpage.css';
 import { useStateValue } from '../StateProvider';
 import UseGoogleSearch from '../hooks/useGoogleSearch';
 import Response from '../constants/response';
+import { Link } from 'react-router-dom';
+import Search from './Search';
+import SearchIcon from '@material-ui/icons/Search';
+import DescriptionIcon from '@material-ui/icons/Description';
+import ImageIcon from '@material-ui/icons/Image';
+import LocalOfferIcon from '@material-ui/icons/LocalOffer';
+import RoomIcon from '@material-ui/icons/Room';
+import MoreVertIcon from '@material-ui/icons/MoreVert'
 
 const SearchPage = () => {
-    const [{term}] = useStateValue();
+    const [{ term }] = useStateValue();
     // Live API call
     // const {data} = UseGoogleSearch(term);
-    
+
     const data = Response;
     console.log(data);
     return (
         <div className='searchPage'>
-            <div className='searchPage__header'> 
-                <h1>{term}</h1>
+            <div className='searchPage__header'>
+                <Link to='/'>
+                    <img className='searchPage__logo'
+                        src='https:www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png'
+                        alt='' />
+                </Link>
+                <div className='searchPage__headerBody'>
+                    <Search hideButtons />
+                    <div className='searchPage__options'>
+                        <div className='searchPage__optionsLeft'>
+                            <div className='searchPage__option'>
+                                <SearchIcon />
+                                <Link to='/all'>All</Link>
+                            </div>
+                            <div className='searchPage__option'>
+                                <DescriptionIcon />
+                                <Link to='/news'>News</Link>
+                            </div>
+                            <div className='searchPage__option'>
+                                <ImageIcon />
+                                <Link to='/images'>Images</Link>
+                            </div>
+                            <div className='searchPage__option'>
+                                <LocalOfferIcon />
+                                <Link to='/shopping'>shopping</Link>
+                            </div>
+                            <div className='searchPage__option'>
+                                <RoomIcon />
+                                <Link to='/maps'>maps</Link>
+                            </div>
+                            <div className='searchPage__option'>
+                                <MoreVertIcon />
+                                <Link to='/more'>more</Link>
+                            </div>
+                        </div>
+                        <div className='searchPage__optionsRight'>
+                            <div className='searchPage__option'>
+                                <Link to='/settings'>Settings</Link>
+                            </div>
+                            <div className='searchPage__option'>
+                                <Link to='/tools'>Tools</Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
 
-            <div className='searchPage__results'> 
-                
-            </div>
+
         </div>
     );
 }
